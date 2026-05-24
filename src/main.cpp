c2f1a535448a48edaf663e156d499ecbaf38414a
BATMAN! <ce1221047@iitd.ac.in>
2026-05-24 11:04:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
+void core(){
+ // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
 
   // Taking User Input
@@ -14,5 +10,15 @@ int main() {
   std::getline(std::cin,command);
 
   std::cout<<command<<": command not found"<<std::endl;
+}
 
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  while (true){
+    core();
+  }
+  return 0;
 }
