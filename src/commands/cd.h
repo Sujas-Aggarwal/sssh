ae62f9045e73c21109081fcf06a76e22f7a42da1
BATMAN! <ce1221047@iitd.ac.in>
2026-05-24 18:51:05 +0530

    codecrafters submit [skip ci]

--- src/commands/cd.h
+++ src/commands/cd.h
@@ -2,15 +2,22 @@
 #include<filesystem>
 #include<string>
 #include <cstdlib>
+#include<iostream>
 
 namespace fs = std::filesystem;
 
 void cd(std::string path){
     // we need to check for absolute and relative paths
-    if (path.substr(0,1)=="~"){
+    try{
+            if (path.substr(0,1)=="~"){
         fs::current_path(std::getenv("HOME"));
     }
     else{
         fs::current_path(path);
     }
+    }
+    catch(...){
+        std::cout << "cd: "<<path<<": No such file or directory"<<std::endl;
+    }
+
 }
